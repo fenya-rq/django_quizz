0984d6c50376956afb3458e8904addc4a35e11fa
Yaroslav Panasenko <fenyochek@gmail.com>
2025-08-04 05:00:38 +0700

    T1.(feat): models added

--- src/apps/assessments/models.py
+++ src/apps/assessments/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from apps.user.models import CustomUser
+
 
 class Quizz(models.Model):
 
@@ -16,6 +18,39 @@ class Question(models.Model):
     quizz = models.ForeignKey(Quizz, blank=True, null=True, on_delete=models.SET_NULL, related_name='questions')
     item = models.CharField(max_length=255)
     item_type = models.CharField(max_length=255, choices=ItemTypes.choices)
-    correct_answer = models.TextField(max_length=255, blank=True, null=True)
+    correct_answer = models.TextField(max_length=255)
 
 
+class CustomUserAnswer(models.Model):
+
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='answers')
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='user_answers')
+    quizz = models.ForeignKey(Quizz, on_delete=models.CASCADE, related_name='user_answers')
+    answer = models.TextField()
+    submitted_at = models.DateTimeField(auto_now_add=True)
+    attempt = models.PositiveIntegerField(default=1)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['user', 'question', 'quizz', 'attempt'],
+                name='unique_answer_per_user_per_question_in_quizz'
+            )
+        ]
+
+
+class CustomUserQuizz(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='user_quizz')
+    quizz = models.ForeignKey(Quizz, on_delete=models.CASCADE, related_name='user_quizz')
+    finished = models.BooleanField(default=False)
+    started_at = models.DateTimeField(auto_now_add=True)
+    finished_at = models.DateTimeField(null=True, blank=True)
+    attempt = models.PositiveIntegerField(default=1)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['user', 'quizz', 'attempt'],
+                name='unique_user_quizz_attempt'
+            )
+        ]
