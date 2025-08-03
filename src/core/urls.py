98513fc2d64d2f170582ff703702b38b3f5e45ca
Yaroslav Panasenko <fenyochek@gmail.com>
2025-08-03 14:04:12 +0700

    feat(apps:user:api): users crud endpoint added

--- src/core/urls.py
+++ src/core/urls.py
@@ -17,6 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from apps.user.api.urls import router as user_router
+
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+urlpatterns += user_router.urls
