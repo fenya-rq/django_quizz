d859b8c90cc6e38a1a518fd291858e7d6eedabe9
Yaroslav Panasenko <fenyochek@gmail.com>
2025-08-05 03:59:25 +0700

    T3 Feat: containerized with nginx + pg + docker

---
+++ src/entrypoint.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+python3 manage.py collectstatic --noinput
+python3 manage.py migrate --noinput
+python3 manage.py createsuperuser --noinput
+gunicorn core.asgi:application -k uvicorn.workers.UvicornWorker -b 0.0.0.0:8000 --workers 4
